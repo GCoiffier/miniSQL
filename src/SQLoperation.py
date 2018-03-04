7c8dcc253005bd822bae054911867ffd1c53a8dc
nchampse <nicolas.champseix@ens-lyon.fr>
2018-03-04 21:59:49 +0100

    try select

--- src/SQLoperation.py
+++ src/SQLoperation.py
@@ -1,5 +1,16 @@
 def select(rel,attributes):
-    pass
+    indices = []
+    for key in attributes :
+        i = 0
+        while rel.keys[i] != key :
+            i += 1
+        indices.append(i)
+    entries = []
+    for line in rel.data :
+        new_line = tuple(line[i] for i in indices)
+        entries.append(new_line)
+    new = Relation("name",attributes,entries)
+    return new
 
 def join(relA,relB,cond=None):
     pass
