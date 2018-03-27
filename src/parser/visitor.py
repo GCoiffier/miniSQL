9ddc182b6613944ba9d38fe2634616994061299c
Guillaume Coiffier <guillaume.coiffier@ens-lyon.fr>
2018-03-27 11:24:22 +0200

    c

--- src/parser/visitor.py
+++ src/parser/visitor.py
@@ -5,6 +5,7 @@ from condition import *
 from antlr4 import *
 from .miniSQLParser import miniSQLParser
 from functools import reduce
+from copy import copy
 
 class Visitor(ParseTreeVisitor):
 
@@ -95,11 +96,12 @@ class Visitor(ParseTreeVisitor):
         if ctx.cond() is not None:
             condTree,_,notIn = self.visit(ctx.cond())
             if notIn:
-                resultNormal = select(select_distinct(resultRelation), Or(condTree), True)
+                r1,r2 = resultRelation, copy(resultRelation)
+                resultNormal = select(r1, Or(condTree), True)
                 print_debug("-------------------------------Le normal : ---------------------------------------")
                 print_debug(resultNormal)
 
-                resultWithIn = select(select_distinct(resultRelation), Or(condTree), False)
+                resultWithIn = select(r2, Or(condTree), False)
                 print_debug("-------------------------------Le withIn : ---------------------------------------")
                 print_debug(resultWithIn)
 
